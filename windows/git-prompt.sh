829e8df158ab75546e1b11d7063be9f6d58386c3
elnurastanov <elnurastanov@gmail.com>
2023-02-04 16:43:58 +0400

    Added branch name to prompt and refactored

--- windows/git-prompt.sh
+++ windows/git-prompt.sh
@@ -1,12 +1,43 @@
 # This conf for windows git bash
 # Will locate this path: "/[disk]/Users/[user]/.config/git/git-prompt.sh
 
-PS1='\[\033]0;Git Bash\007\]'  # set window title
-PS1="$PS1"'\[\033[32m\]'       # change to brownish yellow
-PS1="$PS1"'\W'                 # current working directory
-PS1="$PS1"'\[\033[0m\]'        # change color
-PS1="$PS1"' => '               # prompt: always =>
+# Colors
 
-# This loads nvm (Node Version Manager)
+BLACK="\[\033[1;30m\]"
+RED="\[\033[1;31m\]"
+GREEN="\[\033[1;32m\]"
+YELLOW="\[\033[1;33m\]"
+BLUE="\[\033[1;34m\]"
+PURPLE="\[\033[1;35m\]"
+CYAN="\[\033[1;36m\]"
+WHITE="\[\033[1;37m\]"
+
+RESET_COLOR="\[\033[00m\]"
+
+# Git
+
+BRANCH_NAME=""
+if test -z "$WINELOADERNOEXEC"
+	then
+		GIT_EXEC_PATH="$(git --exec-path 2>/dev/null)"
+		COMPLETION_PATH="${GIT_EXEC_PATH%/libexec/git-core}"
+		COMPLETION_PATH="${COMPLETION_PATH%/lib/git-core}"
+		COMPLETION_PATH="$COMPLETION_PATH/share/git/completion"
+		if test -f "$COMPLETION_PATH/git-prompt.sh"
+		then
+			. "$COMPLETION_PATH/git-completion.bash"
+			. "$COMPLETION_PATH/git-prompt.sh"
+			BRANCH_NAME='`__git_ps1`'
+		fi
+	fi
+
+
+
+# Prompt
+
+PS1="$CYAN\W$RESET_COLOR $YELLOW$BRANCH_NAME$RESET_COLOR $ " 
+
+# Paths
+
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
